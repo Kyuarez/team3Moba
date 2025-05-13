1f9fbe09498aec9acc594958f29eb4d7ee31c7d7
jby0909 <56112547+jby0909@users.noreply.github.com>
2025-05-13 17:10:02 +0900

    [feat] : 타워 적 감지 및 타겟 설정 #5

--- Team3_Moba/Assets/Scripts/Object/Tower/Tower.cs
+++ Team3_Moba/Assets/Scripts/Object/Tower/Tower.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Tower : GameEntity
 {
     public AttackComponent Attack;
     public HealthComponent Health;
+    //private Dictionary<int, GameEntity> enemyDic;
+    private List<GameEntity> enemys;
+    //private int indexTargetEnemy;
 
     private void OnEnable()
     {
@@ -21,7 +25,46 @@ public class Tower : GameEntity
     }
     void Update()
     {
-        // Update tower logic here
-        // For example, check for enemies in range and attack them
+        float minDistance = 10;
+        int targetIndex = -1;
+        for (int i = enemys.Count - 1; i >= 0; i--)
+        {
+            float distance = Vector3.Distance(transform.position, enemys[i].transform.position);
+            
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                targetIndex = i;
+            }
+        }
+        //// Update tower logic here
+        //// For example, check for enemies in range and attack them
+        if (targetIndex > -1)   // 근접한 적의 주소가 감지 된다면 주소를 기반으로 타겟을 정한다.
+        {
+            GameEntity target = enemys[targetIndex];
+            Attack.Attack(target);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            enemys.Add(other.gameObject.GetComponent<GameEntity>());
+            //if(indexTargetEnemy < 0)    //   만약 타워의 타겟이 없다면
+            //{
+            //    indexTargetEnemy = other.gameObject.GetComponent<Enemy>.index;  // 범위 안에 들어온 적의 주소를 획득한다.
+            //}
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            enemys.Remove(other.gameObject.GetComponent<GameEntity>());
+        }
     }
 }
+
