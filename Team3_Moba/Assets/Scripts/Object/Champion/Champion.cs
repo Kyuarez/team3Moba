0e1adcbcbbe9d660d46309e03a498a383aaa1403
Kyuarez <97830742+Kyuarez@users.noreply.github.com>
2025-05-14 11:54:45 +0900

    feat (Champion Animation package import, Champion Animator) #3

--- Team3_Moba/Assets/Scripts/Object/Champion/Champion.cs
+++ Team3_Moba/Assets/Scripts/Object/Champion/Champion.cs
@@ -3,23 +3,59 @@ using UnityEngine.AI;
 
 public class Champion : GameEntity
 {
-    public MoveComponent Move;
+    private Animator championAnimator;
+    private NavMeshAgent agent;
+
+    private float moveSpeed;
+    private float rotateSpeed;
+    private float rotateVelocity;
+
+    private float animSmoothTime = 0.1f;
+
     public AttackComponent Attack;
     public HealthComponent Health;
     public SkillComponent Skill;
 
     private void OnEnable()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        Move = new MoveComponent(transform, agent);
+        championAnimator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
         Attack = new AttackComponent();
         Health = new HealthComponent();
         Skill = new SkillComponent();
+
+        #region Test
+        //@tk : agent ¼¼ÆÃ test
+        moveSpeed = 10f;
+        agent.speed = moveSpeed;
+        agent.angularSpeed = 10000f;
+        agent.acceleration = 10000f;
+        #endregion
+    }
+
+    private void Update()
+    {
+        championAnimator.SetFloat("MoveFactor", agent.velocity.magnitude / agent.speed, animSmoothTime, Time.deltaTime);
     }
 
     public override void InitData(EntityData data)
     {
         base.InitData(data);
-        Move.SetData(data);
+        
+    }
+
+    public void Move(Vector3 destination)
+    {
+        //Move
+        agent.SetDestination(destination);
+        agent.stoppingDistance = 0;
+
+        //Rotate
+        Quaternion rotationToLookat = Quaternion.LookRotation(destination - transform.position);
+        float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+            rotationToLookat.eulerAngles.y, ref rotateVelocity, rotateSpeed * 5f * Time.deltaTime);
+
+        transform.eulerAngles = new Vector3(0f, rotationY, 0f);
     }
 }
